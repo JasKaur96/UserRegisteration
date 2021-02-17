05001bf7ac6ac61c945ef89c1276ae8be0b01305
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-02-17 23:58:50 +0530

    Refactered: Lastname start with capital, min 3 char

--- UserRegisteration/src/com/user/registerationpkg/UserRegisteration.java
+++ UserRegisteration/src/com/user/registerationpkg/UserRegisteration.java
@@ -1,31 +1,26 @@
 package com.user.registerationpkg;
-
-
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegisteration {
 	static Scanner sc= new Scanner(System.in);
 	
-	public static void matches(String reg1) {
-		Pattern p1 = Pattern.compile(reg1);
-//		Matcher m1 = p1.matcher(firstName);
-//		
-//		Matcher m1 = p1.matcher(firstName);
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("\n*****Welocme To User Registeration*****");
-		
-		System.out.println("\nEnter your First Name : ");
-		String firstName=sc.next(); 
-		System.out.println("\nEnter your Last Name : ");
-		String lastName=sc.next(); 
-		System.out.println("\nEnter your Email : ");
-		String email=sc.next(); 
-		
+	public static void validation(String firstName,String lastName) {
 		//To check if name starts with Capital and has min 3 characters.
 		System.out.println("Name: "+Pattern.matches("[A-Z]{1}[a-z]{3,}",firstName));
 		System.out.println("LastName: "+Pattern.matches("[A-Z]{1}[a-z]{3,}",lastName));
 	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n*****Welocme To User Registeration*****");
+		System.out.println("\nEnter your First Name : ");
+		String firstName=sc.next(); 
+		System.out.println("\nEnter your Last Name : ");
+		String lastName=sc.next(); 
+		
+		//Method to validate input data.
+		validation(firstName,lastName);
+	}
 }
+
