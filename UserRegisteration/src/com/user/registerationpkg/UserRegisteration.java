45a82c0d66a183f065d4672147436ee3ca9f7727
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-02-17 23:54:06 +0530

    Refactered: Added comments.

--- UserRegisteration/src/com/user/registerationpkg/UserRegisteration.java
+++ UserRegisteration/src/com/user/registerationpkg/UserRegisteration.java
@@ -1,25 +1,23 @@
 package com.user.registerationpkg;
-
-
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegisteration {
 	static Scanner sc= new Scanner(System.in);
 	
+	public static void validation(String firstName,String lastName,String email,String mobile,String password) {
+		
+		//To check if name starts with Capital and has min 3 characters.
+		System.out.println("Name: "+Pattern.matches("[A-Z]{1}[a-z]{3,}",firstName));
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("\n*****Welocme To User Registeration*****");
-		
-		System.out.println("\nEnter your name : ");
+		System.out.println("\nEnter your First Name : ");
 		String firstName=sc.next(); 
 		
-		//To check if name starts with Capital and has min 3 characters.
-		String reg1="[A-Z]{1}[a-z]{3,}";
-		Pattern p1 = Pattern.compile(reg1);
-		Matcher m = p1.matcher(firstName);
-		boolean m1=m.matches();
-		System.out.println(m1);
+		//Method to validate input data.
+		validation(firstName);
 	}
 }
